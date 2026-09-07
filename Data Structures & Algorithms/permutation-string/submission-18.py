e9c68a1db0169d0fec9f13da03205e7d11eea46e
Matt <141178741+matt0625@users.noreply.github.com>
2026-09-07 22:25:14 +0100

    Add: permutation-string - submission-18

---
+++ Data Structures & Algorithms/permutation-string/submission-18.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        window_size = len(s1)
+        left = 0
+
+        s1_freq = [0] * 26
+        s2_freq = [0] * 26
+
+        for c in s1:
+            index = ord(c) - ord('a')
+            s1_freq[index] += 1
+
+        for right in range(len(s2)):
+            s2_freq[ord(s2[right]) - ord('a')] += 1
+
+            if right - left + 1 > window_size:
+                s2_freq[ord(s2[left]) - ord('a')] -= 1
+                left += 1
+
+            if s1_freq == s2_freq:
+                return True
+        
+        return False
+                    
+            
+
+            
