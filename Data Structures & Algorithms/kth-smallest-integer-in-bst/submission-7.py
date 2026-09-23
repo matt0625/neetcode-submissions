1e029d26f50f1bb949b7353cdaa8df9bbaa7d973
Matt <141178741+matt0625@users.noreply.github.com>
2026-09-23 14:38:09 +0100

    Add: kth-smallest-integer-in-bst - submission-7

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-7.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import heapq
+from collections import deque
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        '''
+        # initial strategy: bfs to reach each node and maintain a max_heap of size k, then the top of the heap is the kth smallest value
+        heap = []
+
+        unvisited = deque([root])
+        while unvisited:
+            current = unvisited.popleft()
+            heapq.heappush_max(heap, current.val)
+            if len(heap) > k:
+                heapq.heappop_max(heap)
+
+            if current.left:
+                unvisited.append(current.left)
+            if current.right:
+                unvisited.append(current.right)
+
+        return heapq.heappop_max(heap)
+        '''
+        # new strategy: inorder traversal until k elements are traversed
+        def inorder(node):
+            nonlocal k
+
+            if not node:
+                return None
+
+            result = inorder(node.left)
+            if result is not None:
+                return result
+
+            k -= 1
+            if k == 0:
+                return node.val
+
+            return inorder(node.right)
+
+        return inorder(root)
+
+
+
+
+        
